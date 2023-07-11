904379ed5a273b3d4db5af9c3a1c0d66f0a80764
Darkatov <atoperez369@gmail.com>
2023-07-11 15:09:19 +0200

    act day1 and add create day4

---
+++ day1/content1.py
@@ -0,0 +1,54 @@
+# ------------------------ DAY 1 ------------------------
+
+# print
+
+"""
+print('Hola Mundo')
+print('Hola Mundo')
+print('Hola Mundo, te digo "hola"')
+print('100 + 50')
+print(100+50)
+"""
+
+# string
+
+"""
+print('Fede')
+print('Hola' + 'Fede')
+print('Hola' + ' ' + 'Fede')
+print('Me llamo \'Federico\'')
+print('Esta es una linea \nY esta es otra linea')
+print('Esta es la tercera linea')
+print('\tEsta es la tercera linea')
+print('This isn´t a number ')
+print('This isn\'t a number ')
+print('Este signo \\ es una barra invertida')
+print('Línea 1\nLínea 2\nLínea 3')
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+print('Barra Normal: /\nBarra Invertida: \\')
+"""
+
+# input
+"""
+print(input('Tu nombre: '))
+print(input('Tu apellido: '))
+print('Tu nombre es ' + input('Tu nombre: '))
+print('Tu nombre es ' + input('Tu nombre: ') + input('Tu apellido'))
+print('Tu nombre es ' + (input('Tu nombre: ')) + ' ' + (input('Tu apellido')))
+print(input('¿Qué estás estudiando?'))
+print(input('¿En qué país vives?'))
+print(input('Escribe tu nombre: ') + ' ' + input('Escribe tu apellido: '))
+"""
+
+# project
+
+print('\nBienvenido al generador para el nombre de tu cerveza ideal.\n'
+      'A continuacion se te haran dos peguntas, \n'
+      'y seguidamente se te mostrara el resultado')
+
+print('\nEl nombre de tu cerveza ideal es:"' + input('\nNombre de tu primera mascota: ') +
+      ' ' + input('Lugar favorito para ir de vacaciones: ') + '"')
+print('\nGracias por preferirnos')
+
+
+
