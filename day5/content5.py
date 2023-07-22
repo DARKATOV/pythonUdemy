d64d2cb607f35171bc262bb533669b6ff09a0ba3
Darkatov <atoperez369@gmail.com>
2023-07-22 04:28:03 +0200

    içnit day5 content

---
+++ day5/content5.py
@@ -0,0 +1,29 @@
+# ------------------------ DAY 5 ------------------------
+
+# functions, methods,
+
+"""
+dic = {'clave1': 100, 'clave2': 500}
+print(dic.popitem())
+print(dic)
+
+x = ',:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#'
+text = x.lstrip()
+print(text)
+
+x = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+modify = x.lstrip(',:%_#')
+print(f'{modify}')
+
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3, 'naranja')
+print(frutas)
+
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+
+conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
+print(conjuntos_aislados)
+"""
+
