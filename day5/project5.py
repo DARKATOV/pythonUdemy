818b4d286e9f43480ff96890ff2030b5fa4400be
Darkatov <atoperez369@gmail.com>
2023-07-28 22:09:33 +0200

    actualizando ejercicio

--- day5/project5.py
+++ day5/project5.py
@@ -1,5 +1,6 @@
 
 from random_word import RandomWords
+import string
 
 r = RandomWords()
 to_word = r.get_random_word()
@@ -13,41 +14,86 @@ print(list_r)
 def tshow():
     lista = []
     counter = 0
+    longitud = len(list_r)
     for lett in list_r:
         if counter == 0:
             lista.append(lett)
             counter += 1
-        elif counter == (to_adivine + 1):
-            lista.append(lett)
+        elif counter < longitud - 1:
+            lista.append('_')
             counter += 1
         else:
-            lista.append('_,')
+            lista.append(lett)
+
     return lista
 
 
 print(tshow())
-show_list = [list_r[0], to_adivine *'_']
 
 
-def correct(letter):
-    for lett in list_r:
-        if lett == letter:
-            show_list.append(lett)
+def enter_letter():
+    lower_check_list = list(string.ascii_lowercase)
+    condicion = False
+    while not condicion:
+        letter = input('Ingresa una letra por favor: ').lower()
+        if len(letter) > 1:
+            print('Solo puedes ingresar una caracter. Intentalo de nuevo')
         else:
-            show_list.append('_')
-    show_list.append(list_r[to_adivine+1])
+            if letter not in lower_check_list:
+                print('Solo se aceptan caracteres alfabeticos. Intentalo de nuevo')
+            else:
+                condicion = True
+    return letter
 
-    return show_list
+
+def verificar_exitencia():
+    lista = []
+    obtenidos = []
+    longitud = len(list_r)
+    counter = 0
+    acert = False
+    lives = 8
+
+    while lives >= 0:
+        print(obtenidos)
+        obtenido = enter_letter()
+        obtenidos.append(obtenido)
+        for lett in list_r:
+            if counter == 0:
+                lista.append(lett)
+                counter += 1
+            elif counter < longitud - 1:
+                if lett in obtenidos:
+                    obtenidos.append(lett)
+                    acert = True
+                    pass
+                else:
+                    lista.append('_')
+                counter += 1
+            else:
+                lista.append(lett)
+
+        if acert:
+            print(f'Has acertado la letra {obtenido}\n {lista}')
+
+        else:
+            print(f'No has acertado con la letra {obtenido}\n {lista}')
+            lives -= 1
+        counter = 0
+        lista.append(enter_letter())
+
+    else:
+        repeat = input(f'No te quedan mas vidas, quieres intentar de nuevo? (S/N)')
+        if repeat.lower() == 's':
+            lives = 8
+            letra_final = enter_letter()
+        else:
+            print('Hasta pronto :D')
 
 
-letra = input('Enter a letter: ')
-letra_lower = letra[0].lower()
+verificar_exitencia()
 
-if letra_lower in list_r:
-    indict = list_r.index(letra_lower)
-    print(correct(letra_lower))
-else:
-    print(show_list)
+
 
 
 
