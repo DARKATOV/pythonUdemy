f60cb26207ab141f57343cd7fccadf045c5cfdb2
Darkatov <atoperez369@gmail.com>
2023-08-09 03:38:11 +0200

    add content day6

---
+++ day6/content6.py
@@ -0,0 +1,16 @@
+# ------------------------ DAY 6 ------------------------
+
+
+# open, read, close
+
+mi_archivo = open('mi.txt')
+print(mi_archivo.read())
+
+print(mi_archivo.readline())
+
+print(mi_archivo.readline().rstrip())
+
+mi_archivo.close()
+
+
+
